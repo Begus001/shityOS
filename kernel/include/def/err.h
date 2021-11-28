8d23b8d68740b2719d195dfa913aa05cd4f8b76d
Begus001 <benjamin.goisser.bg@gmail.com>
2021-11-28 14:19:25 +0100

    Added custom error handlers
    A general handler that outputs a message, dumps all the registers and
    halts
    And an unreachable code handler

---
+++ kernel/include/def/err.h
@@ -0,0 +1,20 @@
+#ifndef KERNEL_ERR_H
+#define KERNEL_ERR_H
+
+#include <tty/tty.h>
+#include <io/serial.h>
+
+_Noreturn static inline void err_halt(char *msg)
+{
+	dbgprintf(msg);
+	tty_set_color(COLOR_BLACK, COLOR_RED);
+	kprintf(msg);
+	__asm__ __volatile__("int 0");
+}
+
+_Noreturn static inline void err_no_reach(void)
+{
+	err_halt("Unreachable code reached\n");
+}
+
+#endif
