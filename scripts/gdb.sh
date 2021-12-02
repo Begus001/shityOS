b5142a78db1754565c4cf9641aa191876ab51551
Begus001 <benjamin.goisser.bg@gmail.com>
2021-12-02 17:18:13 +0100

    Added build and clean scripts manually
    When including an assembly file in another assembly file, both of them
    can't be added to the CMakeLists target sources
    If I then compile, the changes in the included file, which is not in the
    cmake sources, is not recompiled, therefore I just run clean before
    every build
    I wonder why CLion doesn't have this option by default.....

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+./clean.sh
+./build.sh
 ./debug.sh
 
 gdb -ex "lay asm" -ex "lay regs" -ex "focus cmd" -ex "set confirm off" -ex "set pagination off" -ex "target remote :1234" -ex "file ../build/kernel.elf" -ex "b $1" -ex "c"
